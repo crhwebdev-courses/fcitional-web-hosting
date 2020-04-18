f5ef7cc5aad74dfc6f1092280eaac5c83e151bc3
Carl Hain <crhain@gmail.com>
2020-04-18 16:19:28 -0500

    Refactor shared.js to use a closeModal and openModal function; enable clicking on backdrop of modal to close it and also navMenu

--- shared.js
+++ shared.js
@@ -2,23 +2,33 @@ var buttons = document.querySelectorAll(".plan button");
 var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 
-function toggleModal() {
-  modal.classList.toggle("on");
-  backdrop.classList.toggle("on");
+function openModal() {
+  modal.classList.add("on");
+  backdrop.classList.add("on");
+}
+
+function closeModal() {
+  modal.classList.remove("on");
+  backdrop.classList.remove("on");
 }
 
 buttons.forEach(function (el) {
-  el.addEventListener("click", toggleModal);
+  el.addEventListener("click", openModal);
 });
 
 var modalAction = document.querySelectorAll(".modal__action");
 
 modalAction.forEach(function (el) {
-  el.addEventListener("click", toggleModal);
+  el.addEventListener("click", closeModal);
 });
 
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
+
+backdrop.addEventListener("click", function () {
+  mobileNav.classList.remove("expanded");
+  closeModal();
+});
 
 toggleButton.addEventListener("click", function () {
   mobileNav.classList.toggle("expanded");
